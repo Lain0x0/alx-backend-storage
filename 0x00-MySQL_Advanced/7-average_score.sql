63e3120c254e24614b083bc1fc9106708033b8a2
Lain0x0 <lainyamato03@gmail.com>
2024-08-21 19:30:07 +0000

    7-average_score.sql

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,15 @@
+-- script that count average score
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser$$
+CREATE PROCEDURE ComputeAverageScoreForUser(IN users_id INT)
+BEGIN
+    DECLARE average_score FLOAT;
+    SELECT AVG(score) INTO average_score FROM corrections
+    WHERE user_id = users_id;
+
+    UPDATE users
+    SET average_score = average_score
+    WHERE id = users_id;
+END$$
+DELIMITER ;
