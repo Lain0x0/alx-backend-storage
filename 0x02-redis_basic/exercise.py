82389227e80c9e2e4d0eb226eb11a55b2521ef91
Lain0x0 <lainyamato03@gmail.com>
2024-08-28 21:16:59 +0000

    0x1-update

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,3 +18,22 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return (key)
+
+    def get(self, key: str,
+            fn: callable = None) -> Union[str, bytes, int, float]:
+        """ redis data manipulation GET method """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """ redis cache GET methos as string """
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """ redis cache GET method as integer """
+        return self.get(key, int)
